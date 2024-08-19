a8b4c5d6eaa73e56484c8b0fdd42db3859e19a12
Douglas Felipe de Oliveira <douglasfelipedeoliveira@hotmail.com>
2024-08-19 09:23:49 -0300

    Dados do inclinometro no display

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -250,7 +250,8 @@ int main(void)
 		/* USER CODE END WHILE */
 
 		/* USER CODE BEGIN 3 */
-
+		dados_conv();
+		HAL_Delay(100);
 	}
 	/* USER CODE END 3 */
 }
---
+++ Debug/Libs/OLED_Fonts.d
@@ -0,0 +1,2 @@
+Libs/OLED_Fonts.o: ../Libs/OLED_Fonts.c ../Libs/OLED_Fonts.h
+../Libs/OLED_Fonts.h:
